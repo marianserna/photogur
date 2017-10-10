32d9ff3d336015a45a069421e42c5cf138a07ee7
Marian Serna <marianhalliday@gmail.com>
2017-10-10 16:11:11 -0400

    Create new + create actions & new action view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,6 +4,14 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    render text: "Received POST request to '/pictures' with the data URL: #{params.inspect}"
+  end
+
   def show
     @picture = Picture.find(params[:id])
   end
