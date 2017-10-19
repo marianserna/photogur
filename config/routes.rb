392aa41884886f1a39eaaecdebf049b814f3183c
Marian Serna <marianhalliday@gmail.com>
2017-10-19 13:07:10 -0400

    Running users migration + configuring routes as resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
-  get 'users/create'
-
   root 'pictures#index'
-  get 'pictures' => 'pictures#index'
 
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show'
-
-  get 'pictures/:id/edit' => 'pictures#edit'
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy'
+  resources :pictures
+  resources :users, only: [:new, :create]
 end
