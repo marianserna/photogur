4df357d16bf03afdc3cf7e547004e87b8372c63f
Marian Serna <marianhalliday@gmail.com>
2017-10-19 14:05:37 -0400

    Implementing create and destroy actions in sessions controller + adding routes + adding links in layout + creating current_user method in application controller and adding helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find(email: params[:session][:email])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   root 'pictures#index'
 
   resources :pictures
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
