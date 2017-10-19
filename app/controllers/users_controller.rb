9774e57780d93232b523987a9222a5a09b3925cf
Marian Serna <marianhalliday@gmail.com>
2017-10-19 13:41:59 -0400

    Implementing users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "#{@user.email} has been saved!"
+      redirect_to pictures_url
+    else
+      flash[:alert] = "Please fix errors"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
